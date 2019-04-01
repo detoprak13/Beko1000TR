0f039c6a629960d98cc6b5becfe50099277a36b1
deniztoprak <detoprak13@ku.edu.tr>
2019-04-01 23:35:31 +0300

    http requst attempt

--- app/src/main/java/com/dtoprak/beko1000tr/MainActivity.java
+++ app/src/main/java/com/dtoprak/beko1000tr/MainActivity.java
@@ -1,12 +1,24 @@
 package com.dtoprak.beko1000tr;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 	private ImageView homeButton;
@@ -56,6 +68,40 @@ public class MainActivity extends AppCompatActivity {
 				}
 			}
 		});
+
+		OkHttpClient client = new OkHttpClient();
+
+		MediaType mediaType = MediaType.parse("application/json");
+		RequestBody body = RequestBody.create(mediaType, "{\"totalReceiptAmount\":100}");
+		Request request = new Request.Builder()
+				.url("https://sandbox-api.payosy.com/api/get_qr_sale")
+				.post(body)
+				.addHeader("x-ibm-client-id", RetrofitClient.CLIENT_ID)
+				.addHeader("x-ibm-client-secret", RetrofitClient.SECRET_KEY)
+				.addHeader("content-type", "application/json")
+				.addHeader("accept", "application/json")
+				.build();
+		Call call = client.newCall(request);
+		call.enqueue(new Callback() {
+			@Override
+			public void onFailure(Call call, IOException e) {
+				// Something went wrong
+				Log.d("DDDTTT", e.toString());
+			}
+
+			@Override
+			public void onResponse(Call call, Response response) throws IOException {
+				if (response.isSuccessful()) {
+					String responseStr = response.body().string();
+					// Do what you want to do with the response.
+					Log.d("DDDTTT", responseStr);
+				} else {
+					Log.d("DDDTTT", "asd");
+					// Request not successful
+				}
+			}
+		});
+
 	}
 
 }
---
+++ app/src/main/java/com/dtoprak/beko1000tr/RetrofitClient.java
@@ -0,0 +1,34 @@
+package com.dtoprak.beko1000tr;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public class RetrofitClient {
+
+	private static Retrofit retrofit;
+	private static final String BASE_URL = "https://sandbox-api.payosy.com";
+	public static final String CLIENT_ID = "90045867-c788-4a78-82c6-ded0aa6ae9b6";
+	public static final String SECRET_KEY = "lE3pA4tA3tM3yN5aN3sM0vU2mV0uS4rK6rC2nC3jV3gV1rR4bN";
+
+	public static Retrofit getRetrofitInstance() {
+		if (retrofit == null) {
+			retrofit = new retrofit2.Retrofit.Builder()
+					.baseUrl(BASE_URL)
+					.addConverterFactory(GsonConverterFactory.create())
+					.build();
+		}
+		return retrofit;
+	}
+
+	public interface GetDataService {
+
+		@Headers({"x-ibm-client-id: " + CLIENT_ID, "x-ibm-client-secret: " + SECRET_KEY, "content-type", "application/json", "accept", "application/json"})
+		@POST("/get_qr_sale")
+		String getQrData(@Field("totalReceiptAmount") double amount);
+	}
+}
